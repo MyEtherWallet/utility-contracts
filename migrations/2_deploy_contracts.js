0bdaf54e6c4dd6114fa30ea18eb81e503b9fce3e
Kosala Hemachandra <kvhnuke@aol.com>
2018-09-24 22:06:53 -0700

    rewrote the token balance contract

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,37 +1,82 @@
-var Web3 = require("web3")
-var web3 = new Web3()
-var PublicTokens = artifacts.require("PublicTokens")
-var DT1 = artifacts.require("DummyToken")
-var DT2 = artifacts.require("DummyToken")
-var DT3 = artifacts.require("DummyToken")
-var ethTokens = require("../tokens/tokens-eth.json")
-var getHex = (str) => {
-    return '0x' + Buffer.from(str, 'utf8').toString('hex')
-}
+var Web3 = require("web3");
+var web3 = new Web3();
+var PublicTokens = artifacts.require("PublicTokens");
+var TokenBalances = artifacts.require("TokenBalances");
+var DT1 = artifacts.require("DummyToken");
+var DT2 = artifacts.require("DummyToken");
+var DT3 = artifacts.require("DummyToken");
+var DC = artifacts.require("DummyContract");
+var ethTokens = require("../tokens/tokens-eth.json");
+var getHex = str => {
+  return "0x" + Buffer.from(str, "utf8").toString("hex");
+};
 module.exports = function(deployer, network, accounts) {
-    if (network == 'development') {
-        deployer.deploy(PublicTokens).then(function() {
-            return PublicTokens.deployed();
-        }).then(function(pt) {
-            return deployer.deploy([
-                [DT1, accounts[0]],
-                [DT2, accounts[1]],
-                [DT3, accounts[2]]
-            ]).then(function() {
-                return [DT1.deployed(), DT2.deployed(), DT3.deployed()]
-            }).then(function(dTokens) {})
-        })
-    } else if(network == 'live'){
-        deployer.deploy(PublicTokens).then(function() {
-            return PublicTokens.deployed({gas:"0x7a120"})
-        }).then(function(pt) {
-            ethTokens.forEach(async(_token) => {
-                try {
-                    await pt.addSetToken(getHex(_token.name.substr(0, 16)), getHex(_token.symbol), _token.address, _token.decimals, getHex(_token.website), getHex(_token.support.email), {gas:"0x493e0"})
-                } catch (e) {
-                    console.log(_token, e)
-                }
-            })
-        })
-    }
+  if (network == "development") {
+    deployer
+      .deploy(PublicTokens)
+      .then(function() {
+        return PublicTokens.deployed();
+      })
+      .then(function(pt) {
+        return deployer
+          .deploy([[DT1, accounts[0]], [DT2, accounts[1]], [DT3, accounts[2]]])
+          .then(function() {
+            return [DT1.deployed(), DT2.deployed(), DT3.deployed()];
+          })
+          .then(function(dTokens) {
+            return deployer.deploy(TokenBalances, pt.address).then(function() {
+              return deployer.deploy(DC);
+            });
+          });
+      });
+  } else if (network == "live") {
+    deployer
+      .deploy(PublicTokens)
+      .then(function() {
+        return PublicTokens.deployed({ gas: "0x7a120" });
+      })
+      .then(function(pt) {
+        ethTokens.forEach(async _token => {
+          try {
+            await pt.addSetToken(
+              getHex(_token.name.substr(0, 16)),
+              getHex(_token.symbol),
+              _token.address,
+              _token.decimals,
+              getHex(_token.website),
+              getHex(_token.support.email),
+              { gas: "0x493e0" }
+            );
+          } catch (e) {
+            console.log(_token, e);
+          }
+        });
+      })
+      .then(function() {
+        deployer.deploy(TokenBalances, pt.address);
+      });
+  } else if (network == "ropsten") {
+    deployer
+      .deploy(PublicTokens)
+      .then(function() {
+        return PublicTokens.deployed({ gas: "0x7a120" });
+      })
+      .then(function(pt) {
+        ethTokens.forEach(async _token => {
+          try {
+            await pt.addSetToken(
+              getHex(_token.name.substr(0, 16)),
+              getHex(_token.symbol),
+              _token.address,
+              _token.decimals,
+              getHex(_token.website),
+              getHex(_token.support.email),
+              { gas: "0x493e0" }
+            );
+          } catch (e) {
+            console.log(_token, e);
+          }
+        });
+      });
+  }
 };
--- test/PublicTokens.js
+++ test/PublicTokens.js
@@ -1,95 +1,135 @@
-var PublicTokens = artifacts.require("PublicTokens")
-var DummyToken = artifacts.require("DummyToken")
-var Web3 = require('web3')
-var web3 = new Web3()
-var bd = require('../libs/binaryDecoder.js')
+var PublicTokens = artifacts.require("PublicTokens");
+var TokenBalances = artifacts.require("TokenBalances");
+var DummyToken = artifacts.require("DummyToken");
+var DummyContract = artifacts.require("DummyContract");
+var Web3 = require("web3");
+var web3 = new Web3();
+var bd = require("../libs/binaryDecoder.js");
 
 function trim(str) {
-    return str.replace(/\0[\s\S]*$/g, '')
+  return str.replace(/\0[\s\S]*$/g, "");
 }
-var getHex = (str) => {
-    return '0x' + Buffer.from(str, 'utf8').toString('hex')
-}
-contract('PublicTokens', function(accounts) {
-    var pt = null
-    var dt1 = null
-    var dt2 = null
-    var dt3 = null
-    before(async function() {
-        pt = await PublicTokens.deployed()
-        dt1 = await DummyToken.deployed(accounts[1], {
-            from: accounts[1]
-        })
-        dt2 = await DummyToken.new(accounts[2], {
-            from: accounts[2]
-        })
-        dt3 = await DummyToken.new(accounts[3], {
-            from: accounts[3]
-        })
-    })
-    it('should add token contracts', async function() {
-        await pt.addSetToken(
-            getHex("Dummy Token 1"),
-            getHex("DT1"),
-            dt1.address,
-            5,
-            getHex("http://www.dtoken1.eth"),
-            getHex("support@dtoken1.eth"), {
-                from: accounts[0]
-            })
-        console.log(dt1.address)
-        await pt.addSetToken(
-            "Dummy Token 2",
-            "DT2",
-            dt2.address,
-            6,
-            "http://www.dtoken2.eth",
-            "support@dtoken2.eth", {
-                from: accounts[0]
-            })
-        var tokenInfo1 = await pt.getToken(dt1.address)
-        var tokenInfo2 = await pt.getToken(dt2.address)
+var getHex = str => {
+  return "0x" + Buffer.from(str, "utf8").toString("hex");
+};
+contract("PublicTokens", function(accounts) {
+  var pt = null;
+  var tb = null;
+  var dt1 = null;
+  var dt2 = null;
+  var dt3 = null;
+  var dc = null;
+  before(async function() {
+    pt = await PublicTokens.new();
+    console.log(pt.address, "public token address");
+    tb = await TokenBalances.new(pt.address);
+    dt1 = await DummyToken.new(accounts[1], {
+      from: accounts[1]
+    });
+    dt2 = await DummyToken.new(accounts[2], {
+      from: accounts[2]
+    });
+    dt3 = await DummyToken.new(accounts[3], {
+      from: accounts[3]
+    });
+    dc = await DummyContract.new();
+  });
+  it("should add token contracts", async function() {
+    await pt.addSetToken(
+      getHex("Dummy Token 1"),
+      getHex("DT1"),
+      dt1.address,
+      5,
+      getHex("http://www.dtoken1.eth"),
+      getHex("support@dtoken1.eth"),
+      {
+        from: accounts[0]
+      }
+    );
+    await pt.addSetToken(
+      "Dummy Token 2",
+      "DT2",
+      dt2.address,
+      6,
+      "http://www.dtoken2.eth",
+      "support@dtoken2.eth",
+      {
+        from: accounts[0]
+      }
+    );
+    var tokenInfo1 = await pt.getToken(dt1.address);
+    var tokenInfo2 = await pt.getToken(dt2.address);
 
-        assert.equal(trim(web3.toAscii(tokenInfo1[0])), "Dummy Token 1")
-        assert.equal(trim(web3.toAscii(tokenInfo1[1])), "DT1")
-        assert.equal(tokenInfo1[2], dt1.address)
-        assert.equal(tokenInfo1[3].toNumber(), 5)
-        assert.equal(trim(web3.toAscii(tokenInfo1[4])), "http://www.dtoken1.eth")
-        assert.equal(trim(web3.toAscii(tokenInfo1[5])), "support@dtoken1.eth")
+    assert.equal(trim(web3.toAscii(tokenInfo1[0])), "Dummy Token 1");
+    assert.equal(trim(web3.toAscii(tokenInfo1[1])), "DT1");
+    assert.equal(tokenInfo1[2], dt1.address);
+    assert.equal(tokenInfo1[3].toNumber(), 5);
+    assert.equal(trim(web3.toAscii(tokenInfo1[4])), "http://www.dtoken1.eth");
+    assert.equal(trim(web3.toAscii(tokenInfo1[5])), "support@dtoken1.eth");
 
-        assert.equal(trim(web3.toAscii(tokenInfo2[0])), "Dummy Token 2")
-        assert.equal(trim(web3.toAscii(tokenInfo2[1])), "DT2")
-        assert.equal(tokenInfo2[2], dt2.address)
-        assert.equal(tokenInfo2[3].toNumber(), 6)
-        assert.equal(trim(web3.toAscii(tokenInfo2[4])), "http://www.dtoken2.eth")
-        assert.equal(trim(web3.toAscii(tokenInfo2[5])), "support@dtoken2.eth")
-    })
-    it("should fail to register a token from other addresses", async function() {
-        try {
-            var failingTx = await pt.addSetToken(
-                "Dummy Token 3",
-                "DT3",
-                dt3.address,
-                7,
-                "http://www.dtoken3.eth",
-                "support@dtoken3.eth", {
-                    from: accounts[1]
-                })
-            assert.fail("didnt fail the tx")
-        } catch (e) {}
-    });
-    it("should have correct balances", async function() {
-        var balance1 = await dt1.balanceOf(accounts[1])
-        var balance2 = await dt2.balanceOf(accounts[2])
-        assert.equal(balance1.toNumber(), 500000000000000)
-        assert.equal(balance2.toNumber(), 500000000000000)
-    });
-    it("should get correct encoded string", async function() {
-        var allBalance = await pt.getAllBalance(accounts[1], true, true, true, 0)
-        var tokens = bd.decode(allBalance)
-        assert.equal(tokens.length, 2)
-        assert.equal(tokens[0].balance, 500000000000000)
-        assert.equal(tokens[0].symbol, 'DT1')
-        assert.equal(tokens[1].balance, 0)
-    });
-})
+    assert.equal(trim(web3.toAscii(tokenInfo2[0])), "Dummy Token 2");
+    assert.equal(trim(web3.toAscii(tokenInfo2[1])), "DT2");
+    assert.equal(tokenInfo2[2], dt2.address);
+    assert.equal(tokenInfo2[3].toNumber(), 6);
+    assert.equal(trim(web3.toAscii(tokenInfo2[4])), "http://www.dtoken2.eth");
+    assert.equal(trim(web3.toAscii(tokenInfo2[5])), "support@dtoken2.eth");
+  });
+  it("should fail to register a token from other addresses", async function() {
+    try {
+      var failingTx = await pt.addSetToken(
+        "Dummy Token 3",
+        "DT3",
+        dt3.address,
+        7,
+        "http://www.dtoken3.eth",
+        "support@dtoken3.eth",
+        {
+          from: accounts[1]
+        }
+      );
+      assert.fail("didnt fail the tx");
+    } catch (e) {}
+  });
+  it("should have correct balances", async function() {
+    var balance1 = await dt1.balanceOf(accounts[1]);
+    var balance2 = await dt2.balanceOf(accounts[2]);
+    assert.equal(balance1.toNumber(), 500000000000000);
+    assert.equal(balance2.toNumber(), 500000000000000);
+  });
+  it("should get correct encoded string", async function() {
+    var allBalance = await tb.getAllBalance(accounts[1], true, true, true, 0);
+    var tokens = bd.decode(allBalance);
+    assert.equal(tokens.length, 2);
+    assert.equal(tokens[0].balance, 500000000000000);
+    assert.equal(tokens[0].symbol, "DT1");
+    assert.equal(tokens[1].balance, 0);
+  });
+  it("kill one contract and should still work", async function() {
+    await dt2.killMe();
+    var allBalance = await tb.getAllBalance(accounts[1], true, true, true, 0);
+    var tokens = bd.decode(allBalance);
+    assert.equal(tokens.length, 1);
+    assert.equal(tokens[0].balance, 500000000000000);
+    assert.equal(tokens[0].symbol, "DT1");
+  });
+  it("set random contract and should still work", async function() {
+    await pt.addSetToken(
+      "Dummy Contract",
+      "DC",
+      dc.address,
+      6,
+      "http://www.dcontract.eth",
+      "support@dcontract.eth",
+      {
+        from: accounts[0]
+      }
+    );
+    var allBalance = await tb.getAllBalance(accounts[1], true, true, true, 0);
+    var tokens = bd.decode(allBalance);
+    assert.equal(tokens.length, 2);
+    assert.equal(tokens[0].balance, 500000000000000);
+    assert.equal(tokens[0].symbol, "DT1");
+    assert.equal(tokens[1].balance, 0);
+    assert.equal(tokens[1].symbol, "DC");
+  });
+});
